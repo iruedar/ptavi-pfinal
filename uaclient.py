8c085ee94e695660acd82a0a32dcf9cf2402e1d4
ignacio <nachete.rueda@gmail.com>
2018-01-12 10:49:30 +0100

    log acabado en cliente

--- uaclient.py
+++ uaclient.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
@@ -7,11 +8,20 @@ Programa user agent client
 import os
 import sys
 import socket
+import hashlib
 from xml.sax import make_parser
 from uaserver import XMLHandler
 from proxy_registrar import Log
 from xml.sax.handler import ContentHandler
 
+
+def checknonce(nonce):
+
+    fun_check = hashlib.md5()
+    fun_check.update(bytes(nonce, 'utf-8'))
+    fun_check.update(bytes(passwd, 'utf-8'))
+    fun_check.digest()
+    return fun_check.hexdigest()
 
 if __name__ == "__main__":
     try:
@@ -31,6 +41,8 @@ if __name__ == "__main__":
     username = configtags[0][1]['username']
     passwd = configtags[0][1]['passwd']
     uaserv_ip = configtags[1][1]['ip']
+    if uaserv_ip == '':
+        uaserv_ip = "127.0.0.1"
     uaserv_port = str(configtags[1][1]['puerto'])
     audio_port = (configtags[2][1]['puerto'])
     proxy_ip = configtags[3][1]['ip']
@@ -40,51 +52,64 @@ if __name__ == "__main__":
     log = Log(file_log)
     log.start()
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((proxy_ip, proxy_port))
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((proxy_ip, proxy_port))
 
-    if METHOD == 'REGISTER':
-        LINE = METHOD + ' sip:' + username + ':' + uaserv_port
-        LINE += ' SIP/2.0\r\n' + 'Expires:' + OPTION + '\r\n\r\n'
-    elif METHOD == 'INVITE':
-        LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n'
-        SDP = 'Content-Type: application/sdp\r\n\r\n'
-        SDP += 'v=0\r\n' + 'o=' + username + ' ' + uaserv_ip + '\r\n'
-        SDP += 's=sesion\r\n' + 't=0\r\n'
-        SDP += 'm=audio ' + audio_port + ' RTP\r\n\r\n'
-        LINE += SDP
-    elif METHOD == 'BYE':
-        LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n\r\n'
-    else:
-        sys.exit("Peticion Incorrecta")
-    print('Enviando: ' + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    log.sent_to(proxy_ip, proxy_port, LINE)
-    data = my_socket.recv(1024)
-    print(data.decode('utf-8'))
-    receive = data.decode('utf-8').split()
-    log.received_from(proxy_ip, proxy_port, data.decode('utf-8'))
-    if receive[1] == '401':
-        LINE += 'Authorization: Digest response = ' + 'nonce' + '\r\n\r\n'
+        if METHOD == 'REGISTER':
+            LINE = METHOD + ' sip:' + username + ':' + uaserv_port
+            LINE += ' SIP/2.0\r\n' + 'Expires:' + OPTION + '\r\n\r\n'
+        elif METHOD == 'INVITE':
+            LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n'
+            SDP = 'Content-Type: application/sdp\r\n\r\n'
+            SDP += 'v=0\r\n' + 'o=' + username + ' ' + uaserv_ip + '\r\n'
+            SDP += 's=sesion\r\n' + 't=0\r\n'
+            SDP += 'm=audio ' + audio_port + ' RTP\r\n\r\n'
+            LINE += SDP
+        elif METHOD == 'BYE':
+            LINE = METHOD + ' sip:' + OPTION + ' SIP/2.0\r\n\r\n'
+        else:
+            sys.exit("Peticion Incorrecta")
+        print('Enviando: ' + LINE)
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-        data = my_socket.recv(1024)  
-        print('Envio autorizacion: ' + LINE)
-        print(data.decode('utf-8'))
-    elif receive[1] == '100':
-        LINE = 'ACK' + ' sip:' + OPTION + ' SIP/2.0\r\n\r\n'
-        RTP = "./mp32rtp -i 127.0.0.1 -p "  
-        RTP += audio_port + " < " + audio
-        os.system(RTP)
-        print('ejecutando')
-        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        log.sent_to(proxy_ip, proxy_port, LINE)
         data = my_socket.recv(1024)
-        print('Envio ack: ' + LINE)
         print(data.decode('utf-8'))
-    else:
-        pass
+        receive = data.decode('utf-8').split()
+        log.received_from(proxy_ip, proxy_port, data.decode('utf-8'))
+        if receive[1] == '401':
+            nonce = receive[-1].split('=')[1]
+            response = checknonce(nonce)
+            LINE += 'Authorization: Digest response = ' + response + '\r\n\r\n'
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            log.sent_to(proxy_ip, proxy_port, LINE)
+            data = my_socket.recv(1024)  
+            print('Envio autorizacion: ' + LINE)
+            print(data.decode('utf-8'))
+        elif receive[1] == '100':
+            LINE = 'ACK' + ' sip:' + OPTION + ' SIP/2.0\r\n\r\n'
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+            log.sent_to(proxy_ip, proxy_port, LINE)
+            data = my_socket.recv(1024)
+            print('Envio ack: ' + LINE)
+            print(data.decode('utf-8'))
+            RTP = './mp32rtp -i ' + uaserv_ip + ' -p '  
+            RTP += audio_port + " < " + audio
+            log.ejecutando(RTP)
+            print('Ejecutando... ', RTP)
+            os.system(RTP)
+except IndexError:
+    error =  ('SIP/2.0 404 User Not Found\r\n\r\n')
+    log.error(error)
+    sys.exit(error)
 
-    log.finish()
-    print("Terminando socket...")
+except ConnectionRefusedError:
+    error = "No server listening at " + proxy_ip + ' port ' + str(proxy_port)
+    log.error(error)
+    sys.exit(error)
+
+log.finish()
+print("Terminando socket...")
 
 print("Fin.")
